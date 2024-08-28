b31aa75d198921d54232ec6c6803c2bb4f3cb53d
Kelly Willard <kwillard95@gmail.com>
2024-08-28 11:10:14 -0700

    display nato alphanet when user inputs word

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import pandas
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("What word would you like to convert? ")
+formatted_word = user_input.upper()
+coded_content = [nato_alphabet_dict[letter] for letter in formatted_word]
+display = [print(coded_word) for coded_word in coded_content]
 
